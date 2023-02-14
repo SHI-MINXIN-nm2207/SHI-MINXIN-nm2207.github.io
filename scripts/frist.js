07bf9f6c1aef27a26f9c37efd603a7c0a5ed4035
SHI-MINXIN-nm2207 <125366023+SHI-MINXIN-nm2207@users.noreply.github.com>
2023-02-14 20:21:56 +0800

    Add files via upload
    update tem

---
+++ scripts/frist.js
@@ -0,0 +1,11 @@
+
+
+
+function week5() {
+    // b.	Edit your empty function so that it prints “Hello” to the console.log.
+
+    counter = counter + 1;
+
+    document.getElementById("he").innerHTML =  "OK, I have now received " + counter + " clicks";
+  
+  }
